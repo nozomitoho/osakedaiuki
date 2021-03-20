7f0ca35e02b9d99ecbc7b4474062c668e7a285c6
daishikarino <shouyetaizhi@gmail.com>
2021-03-20 21:56:40 +0900

    フォロー機能を付けました

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,27 @@ class User < ApplicationRecord
         has_many :likes, dependent: :destroy
         has_many :liked_games, through: :likes, source: :game
         validates :name, presence: true
-        
+
+        has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
+        has_many :following, through: :following_relationships
+        has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+        has_many :followers, through: :follower_relationships
+
+        #フォローしているかを確認するメソッド
+  def following?(user)
+    following_relationships.find_by(following_id: user.id)
+  end
+
+  #フォローするときのメソッド
+  def follow(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  #フォローを外すときのメソッド
+  def unfollow(user)
+    following_relationships.find_by(following_id: user.id).destroy
+  end
+
         def already_liked?(game)
           self.likes.exists?(game_id: game.id)
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show] 
+  resources :users, only: [:show] do
+      member do
+          get :following, :followers
+      end
+    end
+    resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'games#index'
+  root 'games#top'
+  get "games/top" =>'games#top'
+  get "games/app" =>'games#app'
   resources :games do
     resources :likes, only: [:create, :destroy]
   end
